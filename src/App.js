a93506e3dccf5623e210bb7fd544ea9ba0fc9290
Rifaq.Shaikh <rifaq.shaikh@extentia.com>
2021-10-10 16:18:02 +0530

    aws-config file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Creditor from "./components/Creditor/creditor";
 import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
+import awsconfig from "./aws-config";
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(awsconfig);
